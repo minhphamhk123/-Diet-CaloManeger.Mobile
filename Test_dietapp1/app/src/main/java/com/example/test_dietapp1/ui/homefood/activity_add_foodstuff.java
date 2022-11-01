1c222ec5206db8342148912244da8a519dea8874
thaoht194 <thaohtgm@gmail.com>
2022-11-01 21:31:23 +0700

    edit finish

---
+++ Test_dietapp1/app/src/main/java/com/example/test_dietapp1/ui/homefood/activity_add_foodstuff.java
@@ -0,0 +1,20 @@
+package com.example.test_dietapp1.ui.homefood;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.test_dietapp1.R;
+
+public class activity_add_foodstuff extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        this.setTitle("Tạo thự phẩm mới");
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_foodstuff);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+}
