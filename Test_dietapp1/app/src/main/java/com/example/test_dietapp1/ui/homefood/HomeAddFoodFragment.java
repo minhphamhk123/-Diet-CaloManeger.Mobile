2c10562bcbaaf77a1aa847066aef0a1824b3f5e5
thaoht194 <thaohtgm@gmail.com>
2022-11-16 11:48:53 +0700

    update

--- Test_dietapp1/app/src/main/java/com/example/test_dietapp1/ui/homefood/HomeAddFoodFragment.java
+++ Test_dietapp1/app/src/main/java/com/example/test_dietapp1/ui/homefood/HomeAddFoodFragment.java
@@ -9,9 +9,7 @@ import android.widget.SearchView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentActivity;
 import androidx.viewpager2.widget.ViewPager2;
 
 import com.example.test_dietapp1.R;
