ce1eade02450f5c47d340537cd6b9cea05de9cb1
NGOQUANGKHOA <20521468@gm.uit.edu.vn>
2023-02-02 20:55:28 +0700

    update UI

--- Test_dietapp1/app/src/main/java/com/example/test_dietapp1/ui/notifications/BMRFragment.java
+++ Test_dietapp1/app/src/main/java/com/example/test_dietapp1/ui/notifications/BMRFragment.java
@@ -2,25 +2,14 @@ package com.example.test_dietapp1.ui.notifications;
 
 import android.os.Bundle;
 
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-
-import android.preference.Preference;
-import android.preference.PreferenceFragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import androidx.preference.PreferenceFragmentCompat;
 
 import com.example.test_dietapp1.R;
 
+public class BMRFragment extends PreferenceFragmentCompat {
 
-public class BMRFragment extends Fragment {
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_b_m_r,container,false);
-        return v;
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.root_preferences, rootKey);
     }
 }
-
